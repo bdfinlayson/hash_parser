a2975c8f1096b28a63d0e336b85e2469c72d83c5
Bryan Finlayson <bdfinlayson@gmail.com>
2015-07-07 14:35:11 -0500

    Add solution to code challenge

--- app/interview_1.rb
+++ app/interview_1.rb
@@ -1,5 +1,49 @@
 class Interview1
-  def hello
-    'hello'
+
+  def parse(hash)
+    array = Array.new
+    hash.each do |key, value|
+      if key.kind_of?(Fixnum)
+        array << key.to_s
+      else
+        array << key
+      end
+    end
+    sort_result(array)
+  end
+
+
+  private
+
+  def sort_result(input)
+    input.sort! do |x,y|
+      y.size <=> x.size
+    end
+    return_fixnums(input)
+  end
+
+  def return_fixnums(input)
+    array = Array.new
+    input.each do |str|
+      if str.kind_of?(Symbol)
+        array << str
+      elsif is_integer?(str)
+        array << str.to_i
+      else
+        array << str
+      end
+    end
+    array
+  end
+
+  def is_integer?(str)
+    result = str.split('').select do |char|
+      /\d/.match(char)
+    end
+    if result.size == str.size
+      true
+    else
+      false
+    end
   end
 end
